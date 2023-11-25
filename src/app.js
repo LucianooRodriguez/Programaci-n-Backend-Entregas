685499d39d49b25aeef661aa0c6e226db6404453
Luciano Rodriguez <lucianoorodriguez@outlook.es>
2023-11-25 15:31:35 -0300

    Desafio 4to entregable

--- src/app.js
+++ src/app.js
@@ -1,19 +1,76 @@
 const express = require('express')
-const app = express()
-const PORT = 3000
-const productsRouter = require('../routers/productrouter.js')
-const cartRouter = require('../routers/cartsrouter.js')
+const handlebars = require('express-handlebars')
+const http = require('http')
+const { Server } = require('socket.io')
+const path = require('path')
 
+const productsRouter = require('./routers/products.router.js')
+const cartRouter = require('./routers/carts.router.js')
+const homeRouter = require('./routers/home.router.js')
+
+//esto no va aca
+const { Router } = require('express')
+const router = Router()
+
+// Express and port
+const app = express()
+const PORT = process.env.PORT || 3001
+
+//Http Server 
+const server = http.createServer(app)
+
+//Socket
+const io = new Server(server)
+
+//public
+app.use(express.static(path.resolve(__dirname, "../src/public")))
+
+//Views
+app.engine('handlebars', handlebars.engine())
+app.set('views', path.resolve(__dirname, "../src/views"))
+app.set('view engine', 'handlebars')
+
+/* MULTER */
+//const filesRouter = require('../routers/files.router.js')
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 //Routes
 app.use(productsRouter)
 app.use(cartRouter)
+app.use(homeRouter)
 
-app.listen(PORT, (req,res) => {
-    console.log("Server running on  port ",PORT)
+/* MULTER */
+//app.use(filesRouter)
+
+//Web socket actions
+let messages = []
+
+io.on('connection', (socket) => {
+
+    const ProductManager = require('./classes/ProductManager.js')
+    const prManager = new ProductManager("../products.json")
+
+    //Request Products
+    socket.on("req-products", (data) => { 
+        io.sockets.emit("res-products", { products: prManager.getProducts() })
+    })
+
+    //Cancel New Product
+    socket.on("np-cancel",(data)=>{
+        io.emit("clear-np-screen",{})
+        io.sockets.emit("res-products",{ products: prManager.getProducts() })
+    })
+
+    //Add a New Product
+    socket.on("add-product" , (data) => {
+        prManager.addProduct(data)
+        io.emit("clear-np-screen",{})
+        io.sockets.emit("res-products",{ products: prManager.getProducts() })
+    })
 })
 
+server.listen(PORT, (req, res) => {
+    console.log("Server running on  port ", PORT)
+})
 
--- src/routers/productrouter.js
+++ src/routers/productrouter.js
@@ -1,46 +1,59 @@
 const { Router } = require('express')
 const router = Router()
-
-const productManager = require("../clases/ProductManager.js")
-const Product = require("../clases/Product.js")
+const productManager = require("../classes/ProductManager.js")
+const Product = require("../classes/Product.js")
 const prManager = new productManager("../products.json")
 
+
 //Retorna todos los productos
-router.get('/api/products', (req, res) => {
+router.get('/products', (req, res) => {
     if (req.query.limit) {
-        res.json(prManager.products.slice(0, parseInt(req.query.limit)))
+        res.render('home', {
+            products: prManager.products.slice(0, parseInt(req.query.limit)),
+            style: "style.css"
+        })
     } else {
-        res.json(prManager.getProducts())
+        res.render('home', {
+            products: prManager.getProducts(),
+            style: "style.css"
+        })
     }
+})
+
+router.get('/realTimeProducts', (req, res) => {
+    res.render('realTimeProducts', {
+        style: "style.css"
+    })
 })
 
 //Retorna un producto
 router.get('/api/products/:pid', (req, res) => {
     res.json(prManager.getProductById(parseInt(req.params.pid)))
 })
-
 //Agrega un producto
 router.post('/api/products', (req, res) => {
     let newProduct = new Product(req.body.title, req.body.description, req.body.price, req.body.thumbnail, req.body.code, req.body.status, req.body.stock, req.body.category)
     prManager.addProduct(newProduct)
 })
-
 //Modifica un producto
 router.put('/api/products/:pid', (req, res) => {
     let properties = Object.keys(req.body)
     let newValues = Object.values(req.body)
-
     let i = 0
-
     while (i < properties.length) {
         prManager.updateProductById(req.params.pid, properties[i], newValues[i])
         i++
     }
 })
-
 //Elimina un producto
 router.delete('/api/products/:pid', (req, res) => {
     prManager.deleteProductById(req.params.pid)
+})
+
+//WEBSOCKETS
+
+router.get('/api/realtimeproducts', (req, res) => {
+
 })
 
 module.exports = router;
